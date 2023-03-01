48c596d63e487ae601644ccebf5d5341569c4b46
Anubhav <mishra.anubhav1409@gmail.com>
2023-03-01 21:01:43 +0530

    view post

--- client/src/Components/posts/viewposts.jsx
+++ client/src/Components/posts/viewposts.jsx
@@ -1,9 +1,112 @@
 import React from "react";
 
-export function viewposts(){
-    return(
-        <></>
-    )
+export function viewposts() {
+  return (
+    <>
+      <div className="anubhav">
+        <div class="h-max bg-slate-400 grid grid-col-1 lg:grid-cols-5 md:grid-cols-2 grid-row-4 gap-8 p-16">
+          <div class="bg-slate-200 text-white text-center text-5xl rounded-xl row-span-6 ">
+            Ads
+          </div>
 
+          <div class="bg-white text-black border-slate-300 rounded-xl shadow-lg">
+            <div class="py-2 px-2 rounded-xl overflow-hidden">
+              <img class="rounded-2xl" src="./Images/card.jpg" alt="" />
+            </div>
+            <div>
+              <h3 class="px-4 text-base font-bold">Machine Learning</h3>
+              <p class="px-4 text-xs">Posted by ..... "Username"</p>
+              <hr class="h-px my-4 ml-2 mr-2 bg-gray-200 border-0 dark:bg-gray-700" />
+              <p class="px-4 text-sm mt-3">
+                This work is for machine learning. please do this work. I beg
+                you fucker. do me. my wnajksan da NKJDkjbashdb,msn dmns hsdbmnc
+                dsmn.
+              </p>
+              <div class="mb-4">
+                <button class="bg-slate-300 hover:bg-slate-200 border text-center px-2 py-2 ml-4 mt-5 rounded-xl ">
+                  View Details
+                </button>
+                <button class="bg-slate-300 hover:bg-slate-200  border text-center px-2 py-2 ml-4 mt-5 rounded-xl ">
+                  Apply for job
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white text-black border-slate-300 rounded-xl shadow-lg">
+            <div class="py-2 px-2 rounded-xl overflow-hidden">
+              <img class="rounded-2xl" src="./Images/card.jpg" alt="" />
+            </div>
+            <div>
+              <h3 class="px-4 text-base font-bold">Web Development</h3>
+              <p class="px-4 text-xs">Posted by ..... "Username"</p>
+              <hr class="h-px my-4 ml-2 mr-2 bg-gray-200 border-0 dark:bg-gray-700" />
+              <p class="px-4 text-sm mt-3 ">
+                This work is for machine learning. please do this work. I beg
+                you fucker. do me. my wnajksan da NKJDkjbashdb,msn dmns hsdbmnc
+                dsmn.
+              </p>
+              <div class="mb-4">
+                <button class="bg-slate-300 hover:bg-slate-200 border text-center px-2 py-2 ml-4 mt-5 rounded-xl ">
+                  View Details
+                </button>
+                <button class="bg-slate-300 hover:bg-slate-200  border text-center px-2 py-2 ml-4 mt-5 rounded-xl ">
+                  Apply for job
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white text-black border-slate-300 rounded-xl shadow-lg">
+            <div class="py-2 px-2 rounded-xl overflow-hidden">
+              <img class="rounded-2xl" src="./Images/card.jpg" alt="" />
+            </div>
+            <div>
+              <h3 class="px-4 text-base font-bold">Cyber Security</h3>
+              <p class="px-4 text-xs">Posted by ..... "Username"</p>
+              <hr class="h-px my-4 ml-2 mr-2 bg-gray-200 border-0 dark:bg-gray-700" />
+              <p class="px-4 text-sm mt-3">
+                This work is for machine learning. please do this work. I beg
+                you fucker. do me. my wnajksan da NKJDkjbashdb,msn dmns hsdbmnc
+                dsmn.
+              </p>
+              <div class="mb-4">
+                <button class="bg-slate-300 hover:bg-slate-200 border text-center px-2 py-2 ml-4 mt-5 rounded-xl ">
+                  View Details
+                </button>
+                <button class="bg-slate-300 hover:bg-slate-200  border text-center px-2 py-2 ml-4 mt-5 rounded-xl ">
+                  Apply for job
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white text-black border-slate-300 rounded-xl shadow-lg">
+            <div class="py-2 px-2 rounded-xl overflow-hidden">
+              <img class="rounded-2xl" src="./Images/card.jpg" alt="" />
+            </div>
+            <div>
+              <h3 class="px-4 text-base font-bold">Data Analyst</h3>
+              <p class="px-4 text-xs">Posted by ..... "Username"</p>
+              <hr class="h-px my-4 ml-2 mr-2 bg-gray-200 border-0 dark:bg-gray-700" />
+              <p class="px-4 text-sm mt-3">
+                This work is for machine learning. please do this work. I beg
+                you fucker. do me. my wnajksan da NKJDkjbashdb,msn dmns hsdbmnc
+                dsmn.
+              </p>
+              <div class="mb-4">
+                <button class="bg-slate-300 hover:bg-slate-200 border text-center px-2 py-2 ml-4 mt-5 rounded-xl ">
+                  View Details
+                </button>
+                <button class="bg-slate-300 hover:bg-slate-200  border text-center px-2 py-2 ml-4 mt-5 rounded-xl ">
+                  Apply for job
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default viewposts;
